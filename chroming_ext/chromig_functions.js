0d423c117191b0485ba61f2c7c16f0ab1c23538b
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2023-10-28 01:33:32 +0530

    fixed inline exec and added loading animation

--- chroming_ext/chromig_functions.js
+++ chroming_ext/chromig_functions.js
@@ -1,3 +1,9 @@
+// Edit button handler
+const editButton = document.getElementById("editButton")
+editButton.onclick = async () => {
+    await EditImg();
+};
+
 // Current filter types
 const filter_types = {
     0: "saturate",
@@ -9,15 +15,16 @@ async function createCSS(ftype, fval) {
     const ncss = new StyleSheet()
     ncss.replace(`img { filter: ${filter_types[ftype]}(${fval}) !important; }`)
     document.adoptedStyleSheets = [...document.adoptedStyleSheets, ncss]
+    editButton.setAttribute("aria-busy", "false")
 }
 
 // Main function
 async function EditImg() {
-    console.log("HELLO!")
+    editButton.setAttribute("aria-busy", "true")
     // Get selected filter
     let felemnt = document.getElementById("filter_type")
     let ftype = felemnt.options[felemnt.selectedIndex]
     // Get selected filter value
     let fval = document.getElementById("valrange").value
-    await createCSS(ftype, fval)
+    await createCSS()
 }
